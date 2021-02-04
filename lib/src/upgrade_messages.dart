13a8d42aa24048771cdda25f51e321021044fbe3
ariffmma <75822748+ariffmma@users.noreply.github.com>
2021-02-04 18:38:21 +0700

    Update upgrade_messages.dart

--- lib/src/upgrade_messages.dart
+++ lib/src/upgrade_messages.dart
@@ -154,7 +154,7 @@ class UpgraderMessages {
       case 'en':
       default:
         message =
-            'A new version of {{appName}} is available! Version {{currentAppStoreVersion}} is now available-you have {{currentInstalledVersion}}.';
+           'A new version of {{appName}} Version {{currentAppStoreVersion}} is now available !';
         break;
     }
     return message;
